bed4098c538e4a778550c1e84506073e7d1e1aa5
Srikanth Jambulingam <srikanth.jambulingam@gmail.com>
2022-08-09 14:22:33 +0100

    client changes

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -5,7 +5,7 @@ const BASE_URL = "https://movies-island.herokuapp.com/api/";
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.currentUser;
 const TOKEN = JSON.parse(user)?.accessToken;
-console.log(TOKEN);
+
 export const publicRequest = axios.create({
     baseURL: BASE_URL
 });
